fb3c3415ae755c2ec043220a66904ab79eb0d7bd
atharvx09 <113002246+atharvx09@users.noreply.github.com>
2025-08-06 01:19:47 +0530

    Delete backend directory

--- backend/src/routes/products.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Example route
-router.get("/", (req, res) => {
-  res.send("Product route is working!");
-});
-
-module.exports = router; // ✅ THIS is important
